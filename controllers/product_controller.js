58db82972bdcdaee5c4dccc35ffcc02867aeef0f
Jordan-Burgess <alfredjordan93@gmail.com>
2023-12-10 10:52:14 -0500

    Add Delete route

--- controllers/product_controller.js
+++ controllers/product_controller.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const router = express.Router()
 
+router.use(express.urlencoded({extended: false}))
+
 const products = require('../models/product_model')
 
 router.get('/', (req, res)=>{
@@ -8,15 +10,34 @@ router.get('/', (req, res)=>{
     res.render('index.ejs', data)
 })
 
+router.post('/', (req, res)=>{
+    products.push(req.body)
+    res.redirect('/products')
+})
+
+router.get('/new', (req, res)=>{
+    res.render('new.ejs')
+})
+
 router.get('/:id', (req, res, next)=>{
     if(products[req.params.id] == undefined){
         next()
     }
-    const data = {product: products[req.params.id]}
+    const data = {product: products[req.params.id], id: req.params.id}
     res.render('show.ejs', data)
 }, (req, res)=>{
     const data = {error: req.error}
     res.status(404).render('404.ejs', data)
+})
+
+router.get('/:id/edit', (req, res)=>{
+    const data = {product: products[req.params.id], id: req.params.id}
+    res.render('edit.ejs', data)
+})
+
+router.delete('/:id', (req, res)=>{
+    products.splice(req.params.id, 1)
+    res.redirect('/products')
 })
 
 module.exports = router
--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const app = express()
 const PORT = 4000
+const methodOverride = require('method-override')
 
 app.set('view_engine', 'ejs')
 app.use(express.static('public'))
+app.use(methodOverride('_method'))
 
 const productsController = require('./controllers/product_controller')
 app.use('/products', productsController)
